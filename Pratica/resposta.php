fd1d9c5b2f587b6961f756257c53bf13cebfd783
Cristiano Aguiar <cristianocads@gmail.com>
2024-12-14 14:19:51 -0300

    final pratica 01 Sucessores

--- Pratica/resposta.php
+++ Pratica/resposta.php
@@ -7,10 +7,28 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
+        <nav >
+            <a class="menu" href="index.html">Sucessores</a>
+            <a class="menu" href="#exercicio1">Sorteador</a>
+            <a class="menu" href="#exercicio1">Prática 3</a>
+            <a class="menu" href="#exercicio1">Prática 4</a>
+        </nav>
+    </header>
     <main>
         <h1>Resultado</h1>
         <p>
-            
+            <?php 
+                $n = $_REQUEST["num"] ?? 0;
+                $a = $n - 1;
+                $s = $n + 1;
+                echo "<br>O número escolhido foi: <strong>$n</strong>";
+                echo "<br>O número <em>antecessor</em> é: <strong>$a</strong>";
+                echo "<br>O número <em>sucessor</em> é: <strong>$s</strong></br></br>";
+
+            ?>
+            <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
+            <!--<p><a href="javascript:history.go(-1)">Voltar</a></p>-->
         </p>
     </main>
     

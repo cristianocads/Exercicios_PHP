a7d16ab8b578e97239ee4e1016e4743a444c34c2
Cristiano Aguiar <cristianocads@gmail.com>
2024-12-14 13:49:24 -0300

    pratica de php

---
+++ Pratica/resposta.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Resultado</h1>
+        <p>
+            
+        </p>
+    </main>
+    
+</body>
+</html>
